1346fc54f9ea4915b16af5a0dfd227a0ee839bf4
Soviet.Dog <soviet.dog1945@proton.me>
2023-08-30 21:03:08 +0300

    add updateApiview and serializer

--- ads/serializers/ads.py
+++ ads/serializers/ads.py
@@ -13,7 +13,7 @@ class AdsSerializer(serializers.ModelSerializer):
 class AdsCreateSerializer(serializers.ModelSerializer):
     id = serializers.IntegerField(required=False)
     image = serializers.CharField()
-    user_id = serializers.SlugRelatedField(
+    author_id = serializers.SlugRelatedField(
         queryset=User.objects.all(),
         slug_field='username'
     )
@@ -42,4 +42,43 @@ class AdsCreateSerializer(serializers.ModelSerializer):
         ad.save()
         return ad
 
+
+class AdsUpdateSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(required=False)
+    name = serializers.CharField(required=False, max_length=150)
+    price = serializers.IntegerField(required=False)
+    description = serializers.CharField(required=False)
+    is_published = serializers.BooleanField(required=False)
+    image = serializers.CharField(required=False)
+    author = serializers.SlugRelatedField(
+        required=False,
+        queryset=User.objects.all(),
+        slug_field='username'
+    )
+    category = serializers.SlugRelatedField(
+        required=False,
+        queryset=Category.objects.all(),
+        slug_field='name'
+    )
+
+    def is_valid(self, *, raise_exception=False):
+        self._category = self.initial_data.pop('category_id') if 'category_id' in self.initial_data else None
+        self._author = self.initial_data.pop('author_id') if 'author_id' in self.initial_data else None
+        return super().is_valid(raise_exception=raise_exception)
+
+    def save(self):
+        ad = super().save()
+        try:
+            author_obj = User.objects.get(username=self._author)
+            ad.author_id = author_obj
+
+            category_obj = Category.objects.get(name=self._category)
+            ad.category_id = category_obj
+        finally:
+            ad.save()
+            return ad
+
+    class Meta:
+        model = Ad
+        fields = '__all__'
 
--- ads/views/ads.py
+++ ads/views/ads.py
@@ -1,13 +1,11 @@
-import json
-
 from django.http import JsonResponse
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import UpdateView, DeleteView
-from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView, UpdateAPIView
 
 from ads.models import Ad
-from ads.serializers.ads import AdsSerializer, AdsCreateSerializer
+from ads.serializers.ads import AdsSerializer, AdsCreateSerializer, AdsUpdateSerializer
 
 
 class AdsView(ListAPIView):
@@ -25,32 +23,9 @@ class AdsDetailView(RetrieveAPIView):
     serializer_class = AdsSerializer
 
 
-@method_decorator(csrf_exempt, name='dispatch')
-class AdsUpdateView(UpdateView):
-    model = Ad
-    fields = ['price', 'name', 'description', 'is_published']
-
-    def patch(self, request, *args, **kwargs):
-        super().post(request, *args, **kwargs)
-        ad = self.get_object()
-        new_ad: dict = json.loads(request.body)
-
-        ad.name = new_ad.get('name') or ad.name
-        ad.price = new_ad.get('price') or ad.price
-        ad.description = new_ad.get('description') or ad.description
-        ad.is_published = new_ad.get('is_published') or ad.is_published
-
-        ad.save()
-
-        return JsonResponse({'id': ad.id,
-                             'name': ad.name,
-                             'price': ad.price,
-                             'description': ad.description,
-                             'image': ad.image.url,
-                             'is_published': ad.is_published,
-                             'category_id': ad.category_id.id,
-                             'author_id': ad.author_id.id}
-                            )
+class AdsUpdateView(UpdateAPIView):
+    queryset = Ad.objects.all()
+    serializer_class = AdsUpdateSerializer
 
 
 @method_decorator(csrf_exempt, name='dispatch')
