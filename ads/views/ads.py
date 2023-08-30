0d8309d8e37ed9bc618e38efe89dea22cbaa16be
Soviet.Dog <soviet.dog1945@proton.me>
2023-08-31 01:22:17 +0300

    add price, location and text filter

--- ads/views/ads.py
+++ ads/views/ads.py
@@ -15,15 +15,38 @@ class AdsView(ListAPIView):
 
     def get(self, request, *args, **kwargs):
         ad_category = request.GET.getlist('category', None)
-        category_q = None
+        ad_q = None
         for category in ad_category:
-            if not category_q:
-                category_q = Q(category__id__contains=category)
+            if not ad_q:
+                ad_q = Q(category__id__contains=category)
             else:
-                category_q |= Q(category__id__contains=category)
+                ad_q |= Q(category__id__contains=category)
 
-        if category_q:
-            self.queryset = self.queryset.filter(category_q)
+        text = request.GET.get('text', None)
+        if text:
+            if not ad_q:
+                ad_q = Q(name__icontains=text)
+            else:
+                ad_q |= Q(name__icontains=text)
+
+        location = request.GET.get('location', None)
+        if location:
+            if not ad_q:
+                ad_q = Q(author__location__name__icontains=location)
+            else:
+                ad_q |= Q(author__location__name__icontains=location)
+
+        price_from, price_to = request.GET.get('price_from'), request.GET.get('price_to')
+        if price_to and price_from:
+            if not ad_q:
+                ad_q = Q(price__gte=price_from)
+                ad_q &= Q(price__lte=price_to)
+            else:
+                ad_q |= Q(price__gte=price_from)
+                ad_q &= Q(price__lte=price_to)
+
+        if ad_q:
+            self.queryset = self.queryset.filter(ad_q)
 
         return super().get(request, *args, **kwargs)
 
